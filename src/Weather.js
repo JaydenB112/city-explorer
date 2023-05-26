e9832d84ff2fff6635ad27381f4047ae2cf7dedf
Jayden Boyd <jaydenboydb1@gmail.com>
2023-05-26 18:29:40 -0500

    Yessirski

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 function Weather(props) {
-
+    function WeatherDay({ date, description }) {
+        return (
+          <div>
+            <h3>Date: {date}</h3>
+            <p>Description: {description}</p>
+          </div>
+        );
+      }
     return (
+
         <>
             <h3>{props.forecastData.map((element) => {
                 return (
